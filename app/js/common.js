0c0d21965b3dfe23c16ecb40e2f0ea19a88f57d7
romaaverkin@gmail.com <BeatRolMet1>
2018-01-10 16:13:52 +0700

    Первый коммит.

---
+++ app/js/common.js
@@ -0,0 +1,66 @@
+$(function () {
+
+    $('#my-menu').mmenu({
+        extensions: ['effect-menu-slide', 'pagedim-black'],
+        navbar: {
+            title: 'MoGo'
+        },
+        offCanvas: {
+            position: 'right'
+        }
+    });
+
+    var api = $('#my-menu').data('mmenu');
+    api.bind('open:finish', function () {
+        $('.hamburger').addClass('is-active');
+    });
+    api.bind('close:finish', function () {
+        $('.hamburger').removeClass('is-active');
+    });
+
+    $('.carousel-services').owlCarousel({
+        loop: true,
+        nav: true,
+        smartSpeed: 700,
+        navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+        responsiveClass: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2
+            },
+            1100: {
+                items: 3
+            },
+            1920: {
+                items: 4
+            }
+        }
+    });
+
+    // $('select').selectize();
+
+    //E-mail Ajax Send
+    $("form.callback").submit(function () { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "/mail.php", //Change
+            data: th.serialize()
+        }).done(function () {
+            $(th).find('.success').addClass('active').css('display', 'flex').hide().fadeIn();
+            // alert("Thank you!");
+            setTimeout(function () {
+                // Done Functions
+                th.find('.success').removeClass('active').fadeOut();
+                th.trigger("reset");
+            }, 3000);
+        });
+        return false;
+    });
+
+
+});
