64064f5c5a210407bb98a0a15d7f7a419c59120e
romaaverkin@gmail.com <BeatRolMet1>
2018-01-17 15:50:19 +0700

    Доделал секцию партнеров.

--- app/js/common.js
+++ app/js/common.js
@@ -52,6 +52,32 @@ $(function () {
         navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
     });
 
+    $('.partners').owlCarousel({
+        loop: true,
+        smartSpeed: 700,
+        dots: false,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 4
+            },
+            1200: {
+                items: 5
+            },
+            1920: {
+                items: 6
+            }
+        }
+    });
+
     //E-mail Ajax Send
     $("form.callback").submit(function () { //Change
         var th = $(this);
